3acc989d763443b8b49a6dec0d72d412f2ace155
Aldo Caruso <aldocaruso1991@gmail.com>
2020-02-16 17:15:47 +0100

    esercizio

--- fattoriale.js
+++ fattoriale.js
@@ -1,3 +1,13 @@
 /**
 * Scrivere qui il codice della funzione fattoriale(n)
 **/
+let i = function(j) {
+  var g = 0
+  if (j <= 1){
+    return  1
+    
+  }
+  return g = i(j - 1) * j
+  
+}
+console.log(i(4))
