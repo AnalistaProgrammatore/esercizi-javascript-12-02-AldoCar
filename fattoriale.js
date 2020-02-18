26263552d819aa20750f6079a429205e5d7f762c
Aldo Caruso <aldocaruso1991@gmail.com>
2020-02-18 11:54:39 +0100

    Update con descrizione stack

--- fattoriale.js
+++ fattoriale.js
@@ -2,12 +2,15 @@
 * Scrivere qui il codice della funzione fattoriale(n)
 **/
 let i = function(j) {
-  var g = 0
-  if (j <= 1){
-    return  1
-    
-  }
+    if (j <= 1){
+      return  1
+    }
   return g = i(j - 1) * j
   
 }
 console.log(i(4))
+
+/**stack chiamate ricorsive
+* la funzione j va per 3 volte al return g che a sua volta torna alla funzione j, 
+* dopo tre cicli si verifica la codizione dell'if e dà il valore finale
+**7
